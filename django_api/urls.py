88de5ad5a1626e137b94edf989ef164f93fe8bd3
bibek977 <bhattaraibibek91@gmail.com>
2024-06-22 09:01:47 +0545

    frontend added

--- django_api/urls.py
+++ django_api/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("hello world!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home")
 ]
---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Ensure Vite binds to all network interfaces
+    port: 5173        // Ensure this matches the port in your docker-compose.yml
+  }
+})
